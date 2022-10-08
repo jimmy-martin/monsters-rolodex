37415b3d355d2241d38e3a339ef5e2385e90a84c
jimmy-martin <jimmy.martin952@gmail.com>
2022-10-08 08:10:28 +0200

    how to loop over an array in react component with map

--- src/App.js
+++ src/App.js
@@ -8,49 +8,37 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Jimmy', lastName: 'MARTIN' },
-      company: 'TALEEN',
+      monsters: [
+        {
+          id: 1,
+          name: 'Linda',
+        },
+        {
+          id: 2,
+          name: 'Frank',
+        },
+        {
+          id: 3,
+          name: 'Jacky',
+        },
+        {
+          id: 4,
+          name: 'Andrei',
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            I am {this.state.name.firstName} {this.state.name.lastName} and I
-            work at {this.state.company} !
-          </p>
-          <button
-            onClick={() => {
-              //! Cette façon de faire met à jour le state de façon asynchrone
-              //! donc lorsque je fais un console.log() juste après la modification de mon state,
-              //! je récupère toujours les anciennes valeurs
-              // this.setState({
-              //   name: { firstName: 'Océane', lastName: 'MARTIN' },
-              // });
-              // console.log(this.state);
-
-              //! On va donc procéder ainsi afin de pouvoir tout de suite récupérer les nouvelles valeurs du state
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: 'Océane', lastName: 'MARTIN' },
-                  };
-                },
-                //! On utilise la callback function pour y placer notre console.log()
-                //! Elle sera appelée uniquement lorsque le state aura effectivement été modifié
-                //! et affichera donc les valeurs à jour
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
