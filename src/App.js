0985df3d6208b3a7530fbd9ed2d6c0474018833d
jimmy-martin <jimmy.martin952@gmail.com>
2022-10-08 07:44:22 +0200

    how to update and log the state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Jimmy', lastName: 'MARTIN' },
+      company: 'TALEEN',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            I am {this.state.name.firstName} {this.state.name.lastName} and I
+            work at {this.state.company} !
+          </p>
+          <button
+            onClick={() => {
+              //! Cette façon de faire met à jour le state de façon asynchrone
+              //! donc lorsque je fais un console.log() juste après la modification de mon state,
+              //! je récupère toujours les anciennes valeurs
+              // this.setState({
+              //   name: { firstName: 'Océane', lastName: 'MARTIN' },
+              // });
+              // console.log(this.state);
+
+              //! On va donc procéder ainsi afin de pouvoir tout de suite récupérer les nouvelles valeurs du state
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: 'Océane', lastName: 'MARTIN' },
+                  };
+                },
+                //! On utilise la callback function pour y placer notre console.log()
+                //! Elle sera appelée uniquement lorsque le state aura effectivement été modifié
+                //! et affichera donc les valeurs à jour
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
